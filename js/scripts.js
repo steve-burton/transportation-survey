844dfed6ec6b4de972d41eab5a80ee8448a74b86
Steve-Burton <stevevburton@gmail.com>
2016-10-25 19:49:44 -0700

    add javascript

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+  $("form#transportation-survey").submit(function(event){
+    event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function() {
+      var workTransportationMode = $(this).val();
+      $("#work-responses").append(workTransportationMode + "<br>");
+    });
+    ("#transportation-survey").hide();
+  });
+});
